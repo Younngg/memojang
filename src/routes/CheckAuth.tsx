cbdb665db3ef18ff6b796b01314626f8379dec54
Jiyeong Jeong <8533283@naver.com>
2023-07-19 12:25:05 +0900

    feat: login check 수정

--- src/routes/CheckAuth.tsx
+++ src/routes/CheckAuth.tsx
@@ -1,20 +1,28 @@
-import { Navigate } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
+import { useEffect } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 type Props = {
   children?: React.ReactNode;
 };
 
 const CheckAuth = ({ children }: Props) => {
-  const pathname = window.location.pathname;
-  const token = localStorage.getItem('token');
+  const navigate = useNavigate();
 
-  if (!token && pathname !== '/login') {
-    return <Navigate to='/login' />;
-  }
+  useEffect(() => {
+    const token = localStorage.getItem('token');
 
-  if (pathname === '/login' && token) {
-    return <Navigate to='/' />;
-  }
+    if (!token) {
+      navigate('/login');
+    } else {
+      try {
+        jwtDecode(token);
+        navigate('/');
+      } catch (err) {
+        navigate('/login');
+      }
+    }
+  }, [navigate]);
 
   return children;
 };
